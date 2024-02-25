118db25e60d89cda8fbbb012bffd39c2c68e8f2e
Billy Dunn <bdunn27@gmail.com>
2024-02-25 12:08:03 -0500

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function logCats(cats) {
+    console.log("cats", cats)
+}
+logCats(cats);
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+  }
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){
+    // const copyOfCats = [...cats]
+    // copyOfCats.push(name)
+    // return copyOfCats
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+function removeLastCat(name) {
+    const copyOfCats = cats.slice(0, 2);
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats
+}
